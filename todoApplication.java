1a3dafce6c143953993177687ed7442779c63e84
Shubham Kumar Raj <131697566+shubham2845@users.noreply.github.com>
2023-08-11 21:58:54 +0530

    Create todoApplication.java

---
+++ todoApplication.java
@@ -0,0 +1,12 @@
+package mypackage.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TooDooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TooDooApplication.class, args);
+	}
+}
